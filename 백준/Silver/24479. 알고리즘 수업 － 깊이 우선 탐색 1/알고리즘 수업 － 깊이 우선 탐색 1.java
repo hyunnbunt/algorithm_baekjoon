c1f1017a954ceb2c63e0b973cfbfb48f01c38232
hyunnbunt <103237638+hyunnbunt@users.noreply.github.com>
2024-04-08 21:46:39 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1072 ms, Memory: 104540 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -1,16 +1,20 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    /**
-     입력 받으면서 행렬을 만들기 (2차원 배열)
-     한 쌍의 정점, 간선 u, v에 대해 arr[u][v] 를 1로 설정
-     0부터 시작해서 1인 정점을 방문하고, 해당 정점 인덱스를 기준으로 앞의 과정 반복
-     이때 dfs로 풀이. => 대기열에 방금 넣은 것 (현재 정점의 인접 정점)을 먼저 탐색 (Stack)
-     */
     public static void main(String[] args) throws IOException {
+        /**
+         입력 받으면서 행렬을 만들기 (2차원 배열)
+         한 쌍의 정점, 간선 u, v에 대해 arr[u][v], arr[v][u]를 1로 설정 (양방향 간선)
+         0부터 시작해서 1인 정점을 방문하고, 해당 정점 인덱스를 기준으로 앞의 과정 반복
+         이때 dfs로 풀이. => 대기열에 방금 넣은 것 (현재 정점의 인접 정점)을 먼저 탐색 (Stack)
+         인접 노드를 오름차순으로 방문하려면, 인접 노드를 스택에 내림차순으로 push해야한다
+         => 행렬 메모리 초과
+         => List[]로 다시 구현, 이때 배열에 리스트가 비어있다면 생성해주는 코드 추가 필요
+         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
@@ -37,17 +41,15 @@ public class Main {
         int count = 1;
         while (!stack.isEmpty()) {
             int curr = stack.pop();
-            if (visited[curr]) {
-                continue;
-            }
-            visited[curr] = true;
-            visitedOrder[curr] = count++;
-            if (list[curr] == null) {
-                continue;
-            }
-            list[curr].sort(Comparator.reverseOrder());
-            for (Object next : list[curr]) {
-                stack.push((int) next);
+            if (!visited[curr]) {
+                visited[curr] = true;
+                visitedOrder[curr] = count++;
+                if (list[curr] != null) {
+                    list[curr].sort(Comparator.reverseOrder());
+                    for (Object next : list[curr]) {
+                        stack.push((int) next);
+                    }
+                }
             }
         }
         StringBuilder sb = new StringBuilder();
