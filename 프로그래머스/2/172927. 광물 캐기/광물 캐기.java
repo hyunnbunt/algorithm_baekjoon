b9afeb49a5b0a80d26f1a54a92a12717ac432023
hyuna <103237638+hyunnbunt@users.noreply.github.com>
2024-07-29 11:49:05 +0900

    [level 2] Title: 광물 캐기, Time: 1.35 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,59 +1,66 @@
-
 import java.util.*;
+import java.lang.Math;
 class Solution {
+/** 완전 탐색 => 최소값을 찾아 리턴
+광물 수를 n이라고 할 때, 완전탐색 시간 복잡도 : 3^(n/5)이며 n<=50이므로 => 3^k
+picks에서 유효 곡괭이 선택 후 현재까지의 피로도 합 업데이트 후 다음으로 넘김. 마지막 미네랄의 피로도 계산하는 경우 최소값 업데이트.
+DFS, stack 활용. 곡괭이와 현재 피로도, 깊이 정보를 넣는다 */
+    class Path {
+        int[] leftPicks;
+        int selectedPick;
+        int depth;
+        int stress;
+        Path(int[] leftPicks, int selectedPick, int depth, int stress) {
+            this.leftPicks = leftPicks;
+            this.selectedPick = selectedPick;
+            this.depth = depth;
+            this.stress = stress;
+        }
+    }
     public int solution(int[] picks, String[] minerals) {
-        // 섹션별로 피로도를 정리
-        int[][] picksToStone = new int[][]{{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
-        int m = minerals.length; 
-        int numOfPicks = picks[0] + picks[1] + picks[2];
-        List<int[]> sectionInfo = new ArrayList<>();
-        for (int i = 0; i < Math.min(m, numOfPicks); i ++) {
-            int[] section = new int[3]; // 하나의 섹션에서 각 곡괭이 사용시 피로도 합을 저장
-            for (int j = 0; j < 5; j ++) {
-                int curr = 5 * i + j;
-                if (curr >= m) {
-                    break;
-                }
-                char firstChar = minerals[curr].charAt(0); 
-                int mineral = -1;
-                switch (firstChar) { // 광물의 종류 첫 번째 글자로 구분
-                    case 'd':
-                        mineral = 0;
+        int[][] stressArr = new int[][]{{1, 1, 1}, {5, 1, 1}, {25, 5, 1}}; // 접근 : [곡괭이][광물] 
+        int minStress = Integer.MAX_VALUE; // 초기값
+        int maxMineralIndex = minerals.length-1; // 0~4 : 0, 5~9: 1
+        for (int p = 0; p < 3; p ++) { // 곡괭이 세 가지  
+            if (picks[p] <= 0) continue; // 곡괭이 수가 0이하면 탐색하지 않음
+            Stack<Path> st = new Stack<>(); // 다음 선택지를 차례로 담을 스택 준비
+            Path start = new Path(updatePicks(picks, p), p, 0, 0); // 곡괭이 넘버, 깊이, 피로도 
+            st.push(start); // 유효 곡괭이 중 하나를 스택에 넣고 탐색 시작
+            while (!st.isEmpty()) { 
+                Path curr = st.pop(); 
+                // 현재 곡괭이 선택했을 경우 현재 깊이의 광물 스트레스 계산
+                for (int i = 0; i < 5; i ++) {
+                    int m = curr.depth*5 + i;
+                    if (m > maxMineralIndex) break;
+                    int currMineral = convertToIndex(minerals[m]);
+                    curr.stress += stressArr[curr.selectedPick][currMineral]; // 현재 선택한 곡괭이로 차례인 광물을 캘 때의 피로도 추가
+                    if (m == minerals.length-1) {
+                        minStress = Math.min(minStress, curr.stress);
                         break;
-                    case 'i':
-                        mineral = 1;
-                        break;
-                    case 's':
-                        mineral = 2;
-                        break;
+                    }  // 마지막 광물 피로도 계산 후 최소 피로도 합을 업데이트
                 }
-                for (int pick = 0; pick < 3; pick ++) {
-                   section[pick]+=picksToStone[pick][mineral]; 
+                curr.depth ++;
+                if (curr.depth*5>maxMineralIndex) continue;
+                if (curr.stress >= minStress) continue;
+                for (int nextP = 0; nextP < 3; nextP ++) { // 다음을 스택에 추가
+                    if (curr.leftPicks[nextP] <= 0) continue;
+                    curr.leftPicks = updatePicks(curr.leftPicks, nextP);
+                    curr.selectedPick = nextP;
+                    st.push(curr);
                 }
-            }
-            sectionInfo.add(section);
-        }
-        Collections.sort(sectionInfo, (o1, o2) -> o2[2]-o1[2]);
-        int answer = 0;
-        for (int[] s : sectionInfo) {
-            if (picks[0] > 0) {
-                // 다이아 곡괭이가 있다면 우선 선택
-                answer += s[0];
-                picks[0]--;
-                continue;
-            }
-            if (picks[1] > 0) {
-                // 철 곡괭이가 있다면 다이아 다음으로 우선 선택
-                answer += s[1];
-                picks[1]--;
-                continue;
-            } 
-            if (picks[2] > 0) {
-                answer += s[2];
-                picks[2]--;
             }
         }
-        
-        return answer;
+        return minStress;
+    }
+    public int convertToIndex(String s) {
+        if (s.equals("diamond")) return 0;
+        if (s.equals("iron")) return 1;
+        if (s.equals("stone")) return 2;
+        return -1;
+    }
+    public int[] updatePicks(int[] picks, int currPickIndex) {
+        int[] updated = Arrays.copyOf(picks, 3);
+        updated[currPickIndex]--;
+        return updated;
     }
 }
