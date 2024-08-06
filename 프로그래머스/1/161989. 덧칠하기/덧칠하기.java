33150ad4c277427b1014af854c3322607064a318
hyuna <103237638+hyunnbunt@users.noreply.github.com>
2024-08-06 15:39:45 +0900

    [level 1] Title: 덧칠하기, Time: 1.90 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(int n, int m, int[] section) { 
         int count = 0;
-        int sectionIndex = 0;
         int prevColoredBoundary = 0;
-        while (sectionIndex<section.length) {
-            int s = section[sectionIndex];
-            if (s>prevColoredBoundary) {  
-                prevColoredBoundary = s+m-1;
+        for (int i = 0; i<section.length; i++) {
+            if (section[i]>prevColoredBoundary) {  
+                prevColoredBoundary = section[i]+m-1;
                 count++;
             }
-            sectionIndex++;
         }
         return count;
     }
