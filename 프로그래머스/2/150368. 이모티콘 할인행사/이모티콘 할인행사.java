975f080f77076a125d2dac59d7f0cb1032f615cf
hyuna <103237638+hyunnbunt@users.noreply.github.com>
2024-08-06 18:28:02 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 161.69 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,76 +1,89 @@
 import java.lang.Math;
 import java.util.*;
 class Solution {
-    static int E;
-    static int[] R;
-    static int U;
+    static int numOfEmoticons;
+    static int[] discountRate;
     public class Node {
         int depth;
-        int[] rates;
-        Node(int depth, int[] rates) {
+        int[] prices;
+        Node(int depth, int[] prices) {
             this.depth = depth;
-            this.rates = rates;
+            this.prices = prices;
         }
     }
     public int[] solution(int[][] users, int[] emoticons) {
         /**
-         이모티콘마다 10, 20, 30, 40프로 할인 가능 -> 비율에 따라 사용자들의 행동이 결정됨
-         이모티콘 개수 m에 대해서 4^m만큼의 경우의 수 * n명의 유저, 각 상황의 결과 확인
-         m은 최대 7의 상수이므로, 전체 시간 복잡도는 n. 전부 확인해도 된다.
-         선택가능한 할인율이 적용된 이모티콘의 가격의 배열을 모두 찾기 => DFS => 배열 완성할 때마다 유저 행동 분석
-         */
-        E = emoticons.length;
-        R = new int[]{10, 20, 30, 40};
-        U = users.length;
+        이모티콘마다 10, 20, 30, 40프로 할인 가능
+        그 비율에 따라 사용자들의 행동이 결정됨
+        이모티콘 개수 m : 4*m만큼의 경우의 수 * n명의 유저 각각의 행동 분석 => 전체 결과 리턴
+        m은 최대 7의 상수이므로, 전체 시간 복잡도는 n이라고 할 수 있음. 전부 확인해볼 것.
+        이모티콘의 가격 집합 => 독립적인 경우이며, 각각의 경우마다 user들의 행동을 확인한다
+        가능한 가격 배열 생성하기 DFS
+        */
+        numOfEmoticons = emoticons.length;
+        discountRate = new int[]{10, 20, 30, 40};
         int members = 0;
         int money = 0;
         Stack<Node> st = new Stack<>();
-        st.push(new Node(0, new int[E]));
+        st.push(new Node(0, new int[numOfEmoticons]));
         while(!st.isEmpty()) {
             Node curr = st.pop();
-            int d = curr.depth;
-            if (d == E) {
-                int[] answer = getResult(emoticons, curr.rates, users);
-                if (answer[0] > members || (answer[0] == members && answer[1] > money)) {
-                    members = answer[0];
-                    money = answer[1];
-                }
+            int indexOfEmoticon = curr.depth; // 처음: 0
+            int[] rates = curr.prices; // 처음: {0, 0, 0, 0}
+            if (indexOfEmoticon == numOfEmoticons) {
+                int[] answer = updateAnswer(emoticons, rates, users);
+                members = Math.max(members, answer[0]);
+                money = Math.max(members, answer[1]);
             } else {
-                for (int r : R) {
-                    int[] updated = Arrays.copyOf(curr.rates, E);
-                    updated[d] = r;
-                    st.push(new Node(d+1, updated));
+                for (int dr : discountRate) {
+                    int[] updated = Arrays.copyOf(rates, numOfEmoticons);
+                    updated[indexOfEmoticon] = dr;
+                    st.push(new Node(indexOfEmoticon+1, updated));
+                    for (int r : updated) { 
+                        System.out.print(r + " ");
+                    }
+                    System.out.println("-");
                 }
             }
 
         }
         return new int[]{members, money};
     }
-    public static int[] getResult(int[] emoticons, int[] rates, int[][] users) {
+    public static int[] updateAnswer(int[] prices, int[] rates, int[][] users) {
         int members = 0;
         int money = 0;
-        int[][] userResults = new int[U][2];
-        for (int i = 0; i < U; i ++) {
+        int[][] userResults = new int[users.length][2];
+        for (int i = 0; i < users.length; i ++) {
             int buy = users[i][0];
             int membership = users[i][1];
             int[] userResult = userResults[i];
-            for (int j = 0; j < E; j++) {
+            for (int j = 0; j < rates.length; j++) {
                 int r = rates[j];
                 if (userResult[1] == 1) {
                     break;
                 }
                 if (r>=buy) {
-                    int discounted = emoticons[j]*(100-r)/100;
-                    userResult[0] += discounted;
-                    money += discounted;
+                    userResult[0] += prices[j]*(100-r)/100;
+                    money += prices[j]*(100-r)/100;
                     if (userResult[0]>=membership) {
                         userResult[1] = 1;
-                        members++;
                         money -= userResult[0];
                     }
                 }
             }
         }
+        for (int[] ur : userResults) {
+            if (ur[1] == 1) {
+                members++;
+            } else {
+                money += ur[0];
+            }
+        }
+        for (int r : rates) {
+            System.out.print(r);
+        }
+        System.out.println("-");
+        System.out.println("{" + members + ", " + money + "}");
         return new int[]{members, money};
     }
 }
