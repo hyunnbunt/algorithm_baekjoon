bffb65042116ee74fea5f92ea848dac2a0955620
hyuna <103237638+hyunnbunt@users.noreply.github.com>
2024-08-27 16:04:37 +0900

    [level 1] Title: [1차] 다트 게임, Time: 1.67 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,44 +1,45 @@
 import java.util.*;
 class Solution {
     public int solution(String dartResult) {
-        String[] bonusAndOption = dartResult.split("[0-9]");
-        List<String> bList = new ArrayList<>();
-        for (String b : bonusAndOption) {
-            if (b.length() == 0) {
-                continue;
+        List<String> points = parse(dartResult, "[D, S, T, *, #]");
+        List<String> bonusAndOptions = parse(dartResult, "[0-9]");
+        int[] answer = new int[points.size()];
+        for (int i = 0; i < points.size(); i ++) {
+            int point = Integer.parseInt(points.get(i));
+            char bonus = bonusAndOptions.get(i).charAt(0);
+            if (bonus == 'S') {
+                answer[i] = point;
             }
-            bList.add(b);
-        }
-        String[] points = dartResult.split("[D, S, T, *, #]");
-        List<Integer> pList = new ArrayList<>();
-        for (String p : points) {
-            if (p.length() == 0) {
-                continue;
-            }
-            pList.add(Integer.parseInt(p));
-        }
-        int[] answer = new int[pList.size()];
-        for (int i = 0; i < pList.size(); i ++) {
-            answer[i] = pList.get(i);
-            char bonus = bList.get(i).charAt(0);
             if (bonus == 'D') {
-                answer[i] = (int) Math.pow(answer[i], 2);
-            } else if (bonus == 'T') {
-                answer[i] = (int) Math.pow(answer[i], 3);
+                answer[i] = (int) Math.pow(point, 2);
             }
-            // 옵션 있다면 적용
-            if (bList.get(i).length() > 1) {
-                char option = bList.get(i).charAt(1);
+            if (bonus == 'T') {
+                answer[i] = (int) Math.pow(point, 3);
+            }
+            if (bonusAndOptions.get(i).length() > 1) { // 옵션 있다면
+                char option = bonusAndOptions.get(i).charAt(1);
                 if (option == '*') {
                     answer[i] *= 2;
                     if (i > 0) {
                         answer[i-1] *= 2;
                     }
-                } else if (option == '#') {
+                }
+                if (option == '#') {
                     answer[i] *= -1;
                 }
             }
         }
         return Arrays.stream(answer).sum();
+    }
+    static List<String> parse(String target, String regex) {
+        String[] parsed = target.split(regex);
+        List<String> parsedList = new ArrayList<>();
+        for (String p : parsed) {
+            if (p.length() == 0) {
+                continue;
+            }
+            parsedList.add(p);
+        }
+        return parsedList;
     }
 }
