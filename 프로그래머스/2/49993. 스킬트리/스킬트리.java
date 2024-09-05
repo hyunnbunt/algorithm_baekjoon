58577f99856e326a081ee704de92a2f7aaac3ef9
hyuna <103237638+hyunnbunt@users.noreply.github.com>
2024-09-05 13:07:27 +0900

    [level 2] Title: 스킬트리, Time: 13.66 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,28 +1,19 @@
-import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        Set<Character> set = new HashSet<>();
-        for (char c : skill.toCharArray()) {
-            set.add(c);
-        }
         int answer = 0;
-        for (String st : skill_trees) {
-            int curr = 0;
-            boolean good_st = true;
-            for (char s : st.toCharArray()) { // 현재 스킬
-                if (!set.contains(s)) {
-                    continue;
-                }
-                if (curr >= skill.length() || skill.charAt(curr) != s) { // 이미 모든 스킬 순서대로 나왔거나, 현재 스킬 순서 아닐 때
-                    good_st = false;
-                    break;
-                } else {
-                    curr++;
+        for(String user_skill:skill_trees){
+            String chk = "";
+            for (char s:user_skill.toCharArray()) {
+                for (int i = 0;i<skill.length();i++) {
+                    if (s == skill.charAt(i)) {
+                        chk = chk + s;
+                    }
                 }
             }
-            if (good_st) {
-                answer ++;
+            if(skill.startsWith(chk)) {
+                answer=answer+1;
             }
+
         }
         return answer;
     }
