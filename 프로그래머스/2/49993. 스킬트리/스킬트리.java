435d3c1013ac4ba30554d30cbcb0ad7cab8d2566
hyuna <103237638+hyunnbunt@users.noreply.github.com>
2024-09-05 13:08:00 +0900

    [level 2] Title: 스킬트리, Time: 9.73 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,20 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        for(String user_skill:skill_trees){
-            String chk = "";
-            for (char s:user_skill.toCharArray()) {
-                for (int i = 0;i<skill.length();i++) {
-                    if (s == skill.charAt(i)) {
-                        chk = chk + s;
-                    }
-                }
-            }
-            if(skill.startsWith(chk)) {
-                answer=answer+1;
-            }
+        ArrayList<String> skillTrees = new ArrayList<String>(Arrays.asList(skill_trees));
+        //ArrayList<String> skillTrees = new ArrayList<String>();
+        Iterator<String> it = skillTrees.iterator();
 
+        while (it.hasNext()) {
+            if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
+                it.remove();
+            }
         }
+        answer = skillTrees.size();
         return answer;
     }
 }
