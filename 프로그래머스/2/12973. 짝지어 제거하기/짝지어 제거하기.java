34d5c18486a064101f91ae0aac8ab153fddedcf9
hyuna <103237638+hyunnbunt@users.noreply.github.com>
2024-09-25 20:15:25 +0900

    [level 2] Title: 짝지어 제거하기, Time: 145.75 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i ++) {
+            char c = s.charAt(i);
+            if (st.isEmpty()) {
+                st.push(c);
+            } else if (st.peek() == c) {
+                    st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        
+        return st.isEmpty() ? 1 : 0;
+    }
+}
